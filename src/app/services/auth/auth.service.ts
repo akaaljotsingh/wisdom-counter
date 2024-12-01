e1da545dc341a8579f028565931d42beb22f219a
Akaaljot Singh <akaaljotsingh@gmail.com>
2024-12-01 17:17:08 +0530

    android init

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { GoogleAuth } from 'capacitor-google-auth';
 import { initializeApp } from 'firebase/app';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
@@ -11,6 +12,7 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class AuthService {
+
   constructor(private afAuth: AngularFireAuth) {
     // Initialize Firebase
     // AngularFireModule.initializeApp(environment.firebaseConfig);
@@ -19,19 +21,39 @@ export class AuthService {
   }
 
   // Google login function
+  // async googleLogin() {
+  //   try {
+  //     const provider = new firebase.auth.GoogleAuthProvider();
+  //     await this.afAuth.signInWithRedirect(provider);
+  //     // Handle successful login
+  //   } catch (error) {
+  //     // Handle login errors
+  //   }
+  // }
+
   async googleLogin() {
     try {
-      const provider = new firebase.auth.GoogleAuthProvider();
-      const credential = await this.afAuth.signInWithPopup(provider);
-      // Handle successful login
+      const user = await GoogleAuth.signIn();
+      console.log('Signed in user:', user);
     } catch (error) {
-      // Handle login errors
+      console.error('Google Sign-In Error:', error);
     }
   }
 
   // Logout function
-  logout() {
-    return this.afAuth.signOut();
+  // logout() {
+  //   return this.afAuth.signOut();
+  // }
+  async logout() {
+    // await GoogleAuth.signOut();
+    await this.afAuth.signOut();
+    console.log('User signed out');
+    try {
+      await GoogleAuth.signOut(); // This will sign out the user from Google
+      console.log('User signed out from Google');
+    } catch (error) {
+      console.error('Error signing out from Google:', error);
+    }
   }
 
   getUserDetails(): Observable<any> {
